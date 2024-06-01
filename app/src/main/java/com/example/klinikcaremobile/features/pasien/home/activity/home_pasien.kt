667c85eb090e8423a8b7181f238e37bfa4c84616
TengkuAchmad <achmad.tengku@gmail.com>
2024-06-01 19:27:56 +0700

    Add home pasien layer

---
+++ app/src/main/java/com/example/klinikcaremobile/features/pasien/home/activity/home_pasien.kt
@@ -0,0 +1,21 @@
+package com.example.klinikcaremobile.features.pasien.home.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.klinikcaremobile.R
+
+class home_pasien : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_home_pasien)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
